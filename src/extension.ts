2ca8bcb733558b106c4d6b7febd2785d56d28d82
Icosatess <icosatess@example.com>
2024-09-17 00:37:53 -0400

    Sending update works

--- src/extension.ts
+++ src/extension.ts
@@ -1,17 +1,42 @@
 import * as vscode from 'vscode';
 
-export function activate(context: vscode.ExtensionContext) {
-	console.log('minimapext is running and watching for editor activity');
+const baseUrl = 'http://127.0.0.1:8080';
 
-	const subscription = vscode.window.onDidChangeActiveTextEditor((e) => {
-		if (e === undefined) {
-			console.log('No editor is active');
-		} else {
-      const workspaceFolder = vscode.workspace.getWorkspaceFolder(e.document.uri);
-      console.log(`Active editor is ${e.document.uri} in workspace folder ${workspaceFolder?.name}`);
+const workspaceFolderNameToComponentName = new Map([
+  ['minimapui', 'ui'],
+  ['minimapsrv', 'server'],
+  ['minimapext', 'extension'],
+]);
+
+async function handleDidChangeActiveTextEditor(e: vscode.TextEditor | undefined) {
+  let componentName = '';
+  
+  if (e === undefined) {
+    console.log('No editor is active');
+  } else {
+    const workspaceFolder = vscode.workspace.getWorkspaceFolder(e.document.uri);
+    console.log(`Active editor is ${e.document.uri} in workspace folder ${workspaceFolder?.name}`);
+
+    if (workspaceFolder !== undefined) {
+      componentName = workspaceFolderNameToComponentName.get(workspaceFolder.name) ?? '';
     }
-	});
-	context.subscriptions.push(subscription);
+  }
+
+  try {
+    await fetch(baseUrl + '/component/', {
+      method: 'POST',
+      body: componentName,
+    });
+  } catch (e) {
+    console.error('Failed to send active editor update', e);
+  }
 }
 
-export function deactivate() {}
+export function activate(context: vscode.ExtensionContext) {
+  console.log('minimapext is running and watching for editor activity');
+
+  const subscription = vscode.window.onDidChangeActiveTextEditor(handleDidChangeActiveTextEditor);
+  context.subscriptions.push(subscription);
+}
+
+export function deactivate() { }
